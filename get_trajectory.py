150df968dfb7d101be555ce516c551e3e159ae95
xingguang <qheldiv@gmail.com>
2023-01-13 21:51:08 -0800

    init commit

---
+++ get_trajectory.py
@@ -0,0 +1,207 @@
+
+# %%[markdown]
+# aobj.R and aobj.T are in 
+%matplotlib inline
+%load_ext autoreload
+%autoreload 2
+import numpy as np
+import open3d as o3d
+from open3d.web_visualizer import draw
+import matplotlib.pyplot as plt
+#from xgutils.vis import *
+from kitti360scripts.helpers.annotation import Annotation3D
+from utils import Annotation3D_fixed
+import sys
+import glob
+import open3d as o3d
+import open3d
+from labels import id2label, kittiId2label, name2label
+from labels import labels as kitti_labels
+
+
+kitti360_root = "/localhome/xya120/studio/sherwin_project/KITTI-360"
+sequence = "2013_05_28_drive_0000_sync"
+sys.path.append(kitti360_root)
+poses_data = np.loadtxt("%s/data_poses/%s/poses.txt" % (kitti360_root, sequence))
+
+np.set_printoptions( 3 )
+np.set_printoptions(suppress=True)
+
+poses_matrices = poses_data[:,1:].reshape(-1, 3, 4)
+# %%
+
+#labelDir = '/localhome/xya120/studio/sherwin_project/KITTI-360/data_3d_bboxes/'
+labelDir = '%s/3d_bboxes_full/' % kitti360_root
+#labelDir = '%s/3d_bboxes_full/' % kitti360_root
+labelPath = glob.glob(os.path.join(labelDir, '*', '%s.xml' % sequence))
+#print(os.path.join(labelDir, '*', '%s.xml' % sequence), labelPath)
+#annon = Annotation3D(labelDir=labelDir, sequence=sequence)
+annon = Annotation3D_fixed(labelDir=labelDir, sequence=sequence)
+
+kmeshes = []
+centers = []
+aobjtrans=[]
+labels = []
+for jnd in list(annon.objects.keys())[:]:
+    for ind in annon.objects[jnd].keys():
+        aobj = annon.objects[jnd][ind]
+        #if id2label[aobj.semanticId].name != 'road':
+        #    continue
+        vert, face = aobj.vertices, aobj.faces.astype(np.int32)
+        aobjtrans.append(np.concatenate([aobj.R, aobj.T[:,None]], axis=1))
+        labels.append(aobj.semanticId)
+        #vert = (aobj.vertices-aobj.T) @ aobj.R + aobj.T
+        kmeshes.append([vert, face])
+aobjtrans = np.array(aobjtrans)
+labels = np.array(labels)
+#centers = np.array(centers)
+#center = centers.mean(axis=0)
+#for i in range(centers.shape[0]):
+#    meshes[i].translate(-center)
+#print(center)
+
+#%%
+
+import fresnelvis as fvis
+#from fvis import *
+def testscene(meshes, pcverts=None, vscale=10, lookat=np.array([0.,0.,0.]), camPos=np.array([0,2,0])):
+    camera_kwargs = dict(camPos=np.array([2,2,2])*vscale, camLookat=np.array([0.,0.,0.]),\
+    camUp=np.array([0,1,0]),camHeight=2*vscale, fit_camera=False, \
+    light_samples=32, samples=32, resolution=(256,256))
+
+    renderer = fvis.FresnelRenderer(camera_kwargs=camera_kwargs)
+    for vert, face in meshes:
+        renderer.add_mesh(vert, face, color=np.random.rand(3))
+    #renderer.add_cloud(pcverts, radius=3)
+    img = renderer.render(preview=True)
+    return img
+meshes=[]
+verts = []
+for vert, face in kmeshes[:]:
+    vert = vert 
+    meshes.append([vert, face])
+    verts.append(vert)
+verts = np.concatenate(verts, axis=0)
+for i in range(len(meshes)):
+    meshes[i][0] = meshes[i][0] #- verts.mean(axis=0)
+print(verts.max(axis=0), verts.min(axis=0), verts.mean(axis=0))
+#img = testscene(meshes, pcverts=verts, lookat=meshes[0][0].means(axis=0)*np.array([[1,0,1.]]), 
+#                        camPos=verts.mean(axis=0))
+#img = testscene(meshes, pcverts=verts, lookat=meshes[0][0].mean(axis=0),
+#                        camPos=meshes[0][0].mean(axis=0)+np.array([15,15,15]), vscale=200)
+#plt.imshow(img)
+
+vscale = 80
+cter = (verts.max(axis=0) + verts.min(axis=0))/2
+ater = (poses_matrices[:,:,3].max(axis=0) + poses_matrices[:,:,3].min(axis=0))/2
+camera_kwargs = dict(   camPos=ater+np.array([0,0,150]), 
+                        camLookat=ater,\
+                        camUp=-poses_matrices[0,:3,2],
+                        camHeight=2*vscale, 
+                        fit_camera=False, light_samples=32, samples=32, 
+                        resolution=np.array((256,256))*4
+                        )
+
+renderer = fvis.FresnelRenderer(camera_kwargs=camera_kwargs)
+for i, (vert, face) in enumerate(meshes[:]):
+    color = np.array(id2label[labels[i]].color)/255
+    renderer.add_mesh(vert, face, color= color, solid=1. )
+renderer.add_cloud(aobjtrans[:,:,3], radius=0.3, color=[0,1,0])
+renderer.add_cloud(poses_matrices[:,:,3], radius=.6, color=[1,0,0])
+img = renderer.render(preview=True)
+plt.imshow( img )
+
+#matrices[:,:,3]
+
+m = meshes[0][0].mean(axis=0)
+# %%
+import matplotlib.lines as mlines
+colorlegends =     [mlines.Line2D([], [], color=lg.color, marker='s', linestyle='None',
+                            markersize=10, label=lg.name) for lg in labels]
+plt.legend(handles=colorlegends, loc='upper right')
+plt.show()
+
+
+# %%
+eye = meshes[0].get_center()
+lookat = meshes[1].get_center()
+print(eye, lookat)
+draw([*meshes])#, eye=meshes[0].get_center(), lookat=meshes[1].get_center(), up=[0,1,0])
+# %%
+import open3d as o3d
+
+# Load the 3D object
+mesh = meshes[0]
+
+# Define the camera intrinsic parameters
+intrinsic = o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)
+
+# Define the camera extrinsic parameter, the view point is facing -Z direction
+extrinsic = o3d.geometry.Pose()
+extrinsic.look_at([0,0,0], [0,0,-1], [0,1,0])
+
+# Get the top-view projection
+rgb_image, depth_image = o3d.geometry.create_rgbd_image_from_color_and_depth(mesh, intrinsic, extrinsic, depth_scale=1.0, depth_trunc=1000, convert_rgb_to_intensity=False)
+
+# The projection is in form of 2D image, you can visualize it using openCV or matplotlib
+
+# %%
+import fresnelvis as fvis
+#from fvis import *
+img = fvis.render_test_scene()
+plt.imshow(img)
+# %%
+
+import numpy as np
+import open3d as o3d
+
+points = (np.random.rand(1000, 3) - 0.5) / 4
+colors = np.random.rand(1000, 3)
+
+
+# %%
+import open3d as o3d
+from open3d.web_visualizer import draw
+pc = o3d.geometry.PointCloud()
+pc.points = o3d.utility.Vector3dVector(matrices[:,:,3])
+#pc.normals = o3d.utility.Vector3dVector(np.random.rand(matrices.shape[0],3))
+#cube_red.compute_vertex_normals()
+#cube_red.paint_uniform_color((1.0, 0.0, 0.0))\
+#visualizer = JVisualizer()
+#visualizer.add_geometry(pc)
+print(help(o3d.web_visualizer.draw))
+o3d.web_visualizer.draw(pc)
+#o3d.visualization.draw_geometries([pc])
+#draw(cube_red)
+# %%
+
+o3d.visualization.draw_geometries([pcd],
+                                  zoom=0.3412,
+                                  front=[0.4257, -0.2125, -0.8795],
+                                  lookat=[2.6172, 2.0475, 1.532],
+                                  up=[-0.0694, -0.9768, 0.2024])
+# %%
+import numpy as np
+
+# define the batch of matrices to be multiplied
+matrix_batch = np.array([
+    [[1, 2], [3, 4]],
+    [[5, 6], [7, 8]],
+    [[9, 10], [11, 12]]
+])
+
+# define the matrix to be multiplied with the batch
+multiplier = np.array([[2, 0], [0, 2]])
+
+# perform the batch matrix multiplication
+result = np.matmul(matrix_batch, multiplier)
+
+print(result)
+# %%
+# trash code
+
+        #centers.append( vert.mean(axis=0) )
+        #vert, face = open3d.utility.Vector3dVector(vert), open3d.utility.Vector3iVector(aobj.faces)
+        #mesh = open3d.geometry.TriangleMesh(vertices=vert, triangles=face)
+        #mesh.compute_vertex_normals()
+        #mesh.paint_uniform_color(list(np.random.rand(3)))
---
+++ utils.py
@@ -0,0 +1,128 @@
+from __future__ import print_function, absolute_import, division
+import os
+import json
+from skimage import io, filters
+import numpy as np
+from collections import namedtuple
+from collections import defaultdict
+from matplotlib import cm
+import xml.etree.ElementTree as ET
+import glob
+import struct
+
+# get current date and time
+import datetime
+import locale
+
+# A point in a polygon
+Point = namedtuple('Point', ['x', 'y'])
+
+
+from abc import ABCMeta, abstractmethod
+#from kitti360scripts.helpers.labels     import labels, id2label, kittiId2label, name2label
+from labels    import labels, id2label, kittiId2label, name2label
+MAX_N = 1000
+def local2global(semanticId, instanceId):
+    globalId = semanticId*MAX_N + instanceId
+    if isinstance(globalId, np.ndarray):
+        return globalId.astype(np.int)
+    else:
+        return int(globalId)
+
+def global2local(globalId):
+    semanticId = globalId // MAX_N
+    instanceId = globalId % MAX_N
+    if isinstance(globalId, np.ndarray):
+        return semanticId.astype(np.int), instanceId.astype(np.int)
+    else:
+        return int(semanticId), int(instanceId)
+
+annotation2global = defaultdict()
+
+import sys
+import glob
+import numpy as np
+from kitti360scripts.helpers.annotation import Annotation3D, KITTI360Bbox3D
+
+class Annotation3D_fixed(Annotation3D):
+    # override the init_instance function, to use the override KITTI360Bbox3D.parseBbox function
+    def init_instance(self, labelPath):
+        # load annotation
+        tree = ET.parse(labelPath)
+        root = tree.getroot()
+
+        self.objects = defaultdict(dict)
+        self.objcount = dict()
+
+        self.num_bbox = 0
+
+        for child in root:
+            if child.find('transform') is None:
+                continue
+            obj = KITTI360Bbox3D_fixed() # override the KITTI360Bbox3D.parseBbox function
+            obj.parseBbox(child) # override the KITTI360Bbox3D.parseBbox function
+            if obj.semanticId not in self.objcount:
+                self.objcount[obj.semanticId] = 0
+            if obj.instanceId==-2:
+                obj.instanceId = self.objcount[obj.semanticId]
+            self.objcount[obj.semanticId] += 1
+            
+            globalId = local2global(obj.semanticId, obj.instanceId)
+            self.objects[globalId][obj.timestamp] = obj
+            self.num_bbox+=1
+
+        globalIds = np.asarray(list(self.objects.keys()))
+        semanticIds, instanceIds = global2local(globalIds)
+        for label in labels:
+            if label.hasInstances:
+                print(f'{label.name:<30}:\t {(semanticIds==label.id).sum()}')
+        print(f'Loaded {len(globalIds)} instances')
+        print(f'Loaded {self.num_bbox} boxes')
+classmap = {'driveway': 'parking', 'ground': 'terrain', 'unknownGround': 'ground', 
+                    'railtrack': 'rail track', 'trashbin':"unlabeled"}
+class KITTI360Bbox3D_fixed(KITTI360Bbox3D):
+    def parseBbox(self, child):
+        # try to catch all errors during parsing
+        self.labelname = child.find('label').text
+        if self.labelname in classmap:
+            self.labelname = classmap[self.labelname]
+        try:
+            semanticIdKITTI = int(child.find('semanticId').text)
+            self.semanticId = kittiId2label[semanticIdKITTI].id
+            self.instanceId = int(child.find('instanceId').text)
+            #self.name = kittiId2label[semanticIdKITTI].name
+        except:
+            self.semanticId = name2label[self.labelname].id
+            self.instanceId = -2
+
+        self.start_frame = int(child.find('start_frame').text)
+        self.end_frame = int(child.find('end_frame').text)
+
+        self.timestamp = int(child.find('timestamp').text)
+
+        self.annotationId = int(child.find('index').text) + 1
+
+        global annotation2global
+        annotation2global[self.annotationId] = local2global(self.semanticId, self.instanceId)
+        self.parseVertices(child)
+# %%
+import pandas as pd
+import glob
+import numpy as np
+def count_all_labels(labelDir):
+    labelPaths = glob.glob(os.path.join(labelDir, '*.xml'))
+    dframes = []
+    for labelPath in labelPaths:
+        dframes.append( pd.read_xml(labelPath) )
+    dframe = pd.concat(dframes)
+    dlabels = dframe.label.to_numpy()
+    # also return count of each label
+    print(np.unique(dlabels, return_counts=True))
+    uniquel = np.unique(dlabels)
+    print(dframe["label"].value_counts())
+    return dframe
+    #annotation = Annotation3D_fixed(labelPath)
+    #print(annotation.num_bbox)
+dframe = count_all_labels("/localhome/xya120/studio/sherwin_project/KITTI-360/3d_bboxes_full/train_full")
+
+# %%
